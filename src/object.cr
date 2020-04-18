68c348d3d5b4cf81b2914ce9b7e8c87908175780
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-04-18 20:36:31 +0000

    Refine algorithms to be more in line with the language 'spec'
    While there is no official spec, Crystal currently treats 2
    methods as an override if all the following conditions are met:
    
    - the 2 methods have the same name
    - the 2 methods have the same number of parameters that have
      no default values
    - each parameter of one method has the same type restriction as
      the corresponding parameter in the other method, or has no
      type restriction at all

--- src/object.cr
+++ src/object.cr
@@ -3,9 +3,16 @@ class Object
     macro method_added(method)
       \{% if method.annotation(Override) %}
         \{% if !@type.ancestors.any? &.methods.any? do |m|
-          m.name == method.name &&
-          m.args.map{ |a| r.resolve if r = a.restriction } ==
-            method.args.map{ |a| r.resolve if r = a.restriction }
+          i = 0
+          a1 = m.args.select &.default_value.is_a?(Nop)
+          a2 = method.args.select &.default_value.is_a?(Nop)
+
+          m.name == method.name && a1.size == a2.size && a1.all? do |a|
+            r1 = a.restriction
+            r2 = a2[i].restriction
+            i = i + 1
+            r1.is_a?(Nop) || r2.is_a?(Nop) || r1.resolve == r2.resolve
+          end
         end %}
           \{% raise "Attempt to override non-existent method `\
             #{method.name}(#{method.args.join(", ").id})\
--- src/reference.cr
+++ src/reference.cr
@@ -8,11 +8,18 @@ class Reference
 
     macro method_added(method)
       \{% if @type.ancestors.any? &.methods.any? do |m|
+        i = 0
+        a1 = m.args.select &.default_value.is_a?(Nop)
+        a2 = method.args.select &.default_value.is_a?(Nop)
+
         m.annotation(Final) &&
-        m.name == method.name &&
-        m.args.map{ |a| r.resolve if r = a.restriction } ==
-          method.args.map{ |a| r.resolve if r = a.restriction } &&
-        !m.stringify.starts_with?("abstract ")
+        !m.stringify.starts_with?("abstract ") &&
+        m.name == method.name && a1.size == a2.size && a1.all? do |a|
+          r1 = a.restriction
+          r2 = a2[i].restriction
+          i = i + 1
+          r1.is_a?(Nop) || r2.is_a?(Nop) || r1.resolve == r2.resolve
+        end
       end %}
         \{% raise "Attempt to override final method `\
           #{m.name}(#{m.args.join(", ").id})\
